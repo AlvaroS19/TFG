3e47a70eb10976234fecde01174c6a9a5c4d1852
AlvaroS19 <alvarodev404@gmail.com>
2025-05-12 18:12:59 +0200

    [BACKEND] feat:Conexión con firestore para dar misiones. Closes #22

--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
+
 const express = require('express');
 const cors = require('cors');
-const missionsRoutes = require('./routes/missionsRoutes');
-const userRoutes = require('./routes/userRoutes');
-
-app.use('/missions', missionsRoutes);
-app.use('/user', userRoutes);
-
 require('dotenv').config();
 
+
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
 
-// Rutas
-const authRoutes = require('./routes/auth');
-app.use('/auth', authRoutes);
 
+const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/userRoutes');
-app.use('/user', userRoutes);                     
+const missionsRoutes = require('./routes/missionsRoutes');
+
+
+app.use('/auth', authRoutes);  
+app.use('/user', userRoutes);       
+app.use('/missions', missionsRoutes); 
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-
