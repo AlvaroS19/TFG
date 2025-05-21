8dc45b1eb93974406d935275b031af9a600c8e21
AlvaroS19 <alvarodev404@gmail.com>
2025-05-21 16:43:28 +0200

    Tailwindcss instalado correctamente

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from '../views/LoginView.vue';
 import RegisterView from '../views/RegisterView.vue';
 
+
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: LoginView },
