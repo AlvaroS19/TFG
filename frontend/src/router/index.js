999a9ef302d8a137e68e4746f5a449f55eedde5c
AlvaroS19 <alvarodev404@gmail.com>
2025-05-22 13:09:12 +0200

    [FRONTEND] feat: Creación view reset password. Closes #37

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LandingView  from '@/views/LandingView.vue'
 import LoginView    from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
+import ResetPasswordView from '@/views/ResetPasswordView.vue'
+import ResetPasswordTokenView from '@/views/ResetPasswordTokenView.vue'
 
 const routes = [
   { path: '/',         name: 'Landing',  component: LandingView },
   { path: '/login',    name: 'Login',    component: LoginView },
   { path: '/register', name: 'Register', component: RegisterView },
+  { path: '/reset-password', name: 'ResetPassword', component: ResetPasswordView },
+  { path: '/reset-password/:token', name: 'ResetPasswordToken', component: ResetPasswordTokenView },
 ]
 
 export default createRouter({
