54a8de9c2ed713373554c4ee614e0ed0db0f5e3f
AlvaroS19 <alvarodev404@gmail.com>
2025-05-22 18:02:36 +0200

    [FRONTEND] feat: Backend-Fronend vinculados. Closes #39

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -24,5 +24,14 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
